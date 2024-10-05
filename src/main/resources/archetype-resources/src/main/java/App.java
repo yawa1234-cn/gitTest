8cdf4712dd766aef3a1a3849edad4bc3472c8082
yangchangxing <1186983020@qq.com>
2024-10-05 20:46:56 +0800

    push commit

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println( "Hello git4!" );
         System.out.println( "master test!" );
         System.out.println( "hotfix test!" );
+        System.out.println( "push test!" );
     }
 }
